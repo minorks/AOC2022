6431cac2ab86e4ad0f8f2cb54aad50783b739126
minorks <113790374+minorks@users.noreply.github.com>
2022-12-08 17:02:38 -0500

    More day 8 stuff

--- day8_v2.py
+++ day8_v2.py
@@ -1,32 +1,29 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec  8 07:33:27 2022
+Created on Thu Dec  8 14:24:12 2022
 
-@author: hokie
+@author: kyle.minor
 """
-from pandas import read_fwf
+
+from numpy import array
+from numpy import amax
 
 def getSubs(tbl,cell):
-    return [tbl.iloc[cell[0]:cell[0]+1,:cell[1]],   #Left
-            tbl.iloc[cell[0]:cell[0]+1,cell[1]+1:],   #Right
-            tbl.iloc[:cell[0],cell[1]:cell[1]+1],   #Up
-            tbl.iloc[cell[0]+1:,cell[1]:cell[1]+1]]   #Down
-
-def checkVis(tbl,cell):    
-    subs = getSubs(tbl,cell)
-    val = tbl.iloc[cell[0],cell[1]]
+    return [tbl[cell[0]:cell[0]+1,:cell[1]],   #Left
+            tbl[cell[0]:cell[0]+1,cell[1]+1:],   #Right
+            tbl[:cell[0],cell[1]:cell[1]+1],   #Up
+            tbl[cell[0]+1:,cell[1]:cell[1]+1]]   #Down
     
+def checkVis(subs,val):    
     for x in subs:
-        if max(x.max()) < val: return True
-    
+        if amax(x) < val: return True
     return False  
 
-def getScore(tbl,cell):
-    subs = getSubs(tbl,cell)
+def getScore(subs,val):
     scores = [0 for x in range(0,4)]
-    val = tbl.iloc[cell[0],cell[1]]
     
     # Left Subscore
+    for i in range(0,subs[0].shape[])
     for i in range(0,len(subs[0].values.toList())):
         comp = subs[0].iloc[0,len(subs[0])-(i+1)]
         if comp < val: 
@@ -64,35 +61,30 @@ def getScore(tbl,cell):
         
     print (scores)
     return(scores[0]*scores[1]*scores[2]*scores[3])
-    
-        
 
-#file = open("C:\\Users\\hokie/Documents/Programming/AOC22/day8_input.txt")
 file = open("C:/Users/kyle.minor/PythonStuff/AOC2022/AOC2022/" \
-               "Day8_testInput.txt")
-cols = len(file.readline().strip())
-
-tbl = read_fwf("C:/Users/kyle.minor/PythonStuff/AOC2022/AOC2022/" \
-               "Day8_testInput.txt",widths=[1 for x in range(0,cols)], \
-               header=None)
+               "Day8_input.txt")
+lines = file.readlines()
+for i in range(0,len(lines)):
+    lines[i] = list(lines[i].strip())
+    for j in range(0,len(lines[i])):
+        lines[i][j] = int(lines[i][j])
+        
+tbl = array(lines)
 
 visCt = 0
 score = 0
-for i in range(0,len(tbl)):      # By Rows
-    for j in range(0,len(tbl.columns)):   # By Columns    
-        if (i == 0 or i == len(tbl)-1) or (j == 0 or j == len(tbl.columns)-1):
+for i in range(0,tbl.shape[0]):      # By Rows
+    for j in range(0,tbl.shape[1]):   # By Columns    
+        if (i == 0 or i == tbl.shape[0]-1) or (j == 0 or j == tbl.shape[1]-1):
             visCt += 1
         else:
-            if checkVis(tbl=tbl,cell=[i,j]):
+            subs = getSubs(tbl=tbl,cell=[i,j])
+            if checkVis(subs=subs,val=tbl[i,j]):
                 visCt += 1
-            tScore = getScore(tbl,cell=[i,j])
+            tScore = getScore(subs=subs,val=tbl[i,j])
             print (tScore)
             if tScore > score:
                 score = tScore
 
 print("Part 1: ", visCt)
-print("Part 2: ", tScore)
-
-
-
-
